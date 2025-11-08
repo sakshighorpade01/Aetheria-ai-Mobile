22d7586c96e9f938a3778f3b4505717e2a8dfaf5
Prajwal Ghadge <prajwalghadge2005@gmail.com>
2025-11-09 02:50:35 +0530

    version update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Production Service Worker for Aetheria AI PWA
-const CACHE_VERSION = 'aetheria-v1.0.5';
+const CACHE_VERSION = 'aetheria-v1.0.6';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
@@ -27,6 +27,7 @@ const STATIC_ASSETS = [
   '/css/aios.css',
   '/css/mobile.css',
   '/css/modals.css',
+  '/css/install-prompt.css',
   
   // Core JS modules
   '/js/config.js',
